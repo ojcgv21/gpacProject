4add4481e854ea170c23efdcb22f90027297d7fa
Oscar <oscar@ubuntu.ubuntu-domain>
2014-09-29 03:04:38 +0200

    bug

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
  document.getElementById("nav01").innerHTML =
 "<ul id='menu'>" +
 "<li><a href='commandes.php'>COMMANDES A EXECUTER</a></li>" +
-"<li><a href='lcov/index.html'>COUVERTURE DU CODE</a></li>" +
+"<li><a href='lcovresult/index.html'>COUVERTURE DU CODE</a></li>" +
 "</ul>"; 
